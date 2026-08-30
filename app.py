9c2bab624756e0d8627abe446b2732391ea83922
fluidpotata <md.ibrahim.alif@g.bracu.ac.bd>
2026-08-30 21:36:53 +0600

    Moduler App file

--- app.py
+++ app.py
@@ -1,39 +1,48 @@
 from flask import Flask, render_template, request
 
-from classical.substitution import Substitution
-from classical.double_transposition import DoubleTransposition
-
-from symmetric.des import DES
-from symmetric.aes import AES
-
-from asymmetric.rsa import RSA
-from asymmetric.ecc import ECC
+from utils.substitution_handler import handle_substitution
+from utils.transposition_handler import handle_double_transposition
+# from utils.des_handler import handle_des
+# from utils.aes_handler import handle_aes
+# from utils.rsa_handler import handle_rsa
+# from utils.ecc_handler import handle_ecc
 
 
 app = Flask(__name__)
 
 
+handlers = {
+    "substitution": handle_substitution,
+    "double_transposition": handle_double_transposition,
+    # "des": handle_des,
+    # "aes": handle_aes,
+    # "rsa": handle_rsa,
+    # "ecc": handle_ecc
+}
+
+
 @app.route("/", methods=["GET", "POST"])
 def index():
     result = None
+    error = None
 
     if request.method == "POST":
         algorithm = request.form.get("algorithm")
+        try:
+            handler = handlers.get(algorithm)
 
-        if algorithm == "substitution":
-            pass
-        elif algorithm == "double_transposition":
-            pass
-        elif algorithm == "des":
-            pass
-        elif algorithm == "aes":
-            pass
-        elif algorithm == "rsa":
-            pass
-        elif algorithm == "ecc":
-            pass
+            if handler is None:
+                raise ValueError("Invalid algorithm selected.")
+            result = handler(request.form)
 
-    return render_template("index.html",result=result)
+        except ValueError as e:
+            error = str(e)
+
+    return render_template(
+        "index.html",
+        result=result,
+        error=error
+    )
 
 
 if __name__ == "__main__":
